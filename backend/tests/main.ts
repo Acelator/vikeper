b3596ebb6fd0d6c97313aef50fe3494f7e99f6cc
Pablo <Corbla04@gmail.com>
2020-08-19 16:57:27 +0200

    Set stage for saving test results in file

--- backend/tests/main.ts
+++ backend/tests/main.ts
@@ -1,23 +1,39 @@
-import {serverTest} from "./server";
+import fs from "fs"
+
+// Import tests
+import {getServer} from "./server";
 
 class TestSuite {
+	// Gets the current date with the format <yyyyMMddHHmmss>
+	private date = new Date();
+	private time = `${this.date.getFullYear()}$${this.date.getMonth()}${this.date.getDay()}${this.date.getHours()}${this.date.getMinutes()}${this.date.getSeconds()}`
+	private logFileName: any = this.time.replace("$", "0");
+
 	constructor(appInit: {tests: any}) {
 		this.test(appInit.tests);
+		this.log();
 	}
 
-	public test(tests: {forEach: (arg0: (test: any) => any) => any}) {
+	// TODO: SAVE RESPONSE TO A FILE
+	private test(tests: {forEach: (arg0: (test: any) => any) => any}) {
 		tests.forEach(test => {
 			test()
-				.then(function (f: boolean) {
-					return f;
+				.then(function (response: any) {
+					console.log(response);
 				})
 				.catch(function (err: any) {
 					console.log(err);
 				});
 		});
 	}
+
+	// TODO: PRINTS THE TESTS RESULTS
+	private log(): void {
+		console.log(this.logFileName);
+	}
 }
 
-const testSuite = new TestSuite({
-	tests: [serverTest],
+// Create a new instance of the App
+new TestSuite({
+	tests: [getServer],
 });
--- backend/tests/server.ts
+++ backend/tests/server.ts
@@ -1,8 +1,22 @@
 const axios = require("axios").default;
 
-export async function serverTest() {
+export async function getServer() {
+	const response = {
+		number: 1,
+		it: "Tests that the server is running",
+		description: "Basic GET request to the base api URL",
+		testInfo: {
+			serverPath: "",
+			statusText: "",
+			statusCode: "",
+			expectedStatusCode: "",
+		},
+		testPassed: "",
+	};
+	const url: string = "http://localhost:3000";
+	const serverPath: string = "/api/v1/tasks/s";
 	const req = await axios
-		.get("http://localhost:3000/api/v1/tasks/s")
+		.get(`${url}${serverPath}`)
 		.then(function (response: any) {
 			return response;
 		})
@@ -12,8 +26,14 @@ export async function serverTest() {
 
 	if (req) {
 		if (req.status === 200) {
-			const f = true;
-			return f;
+			// Creates the response object
+			response.testInfo.serverPath = serverPath;
+			response.testInfo.statusCode = req.status;
+			response.testInfo.statusText = req.statusText;
+			response.testInfo.expectedStatusCode = "200";
+			response.testPassed = "true";
+
+			return response;
 		} else if (req.status !== 200) {
 			return false;
 		}
